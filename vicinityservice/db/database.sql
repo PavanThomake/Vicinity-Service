ddd302d15e4abab9b8bc4803c9f59d091b1ac5a5
Pavan Thomake <168662302+PavanThomake@users.noreply.github.com>
2024-05-02 23:56:34 +0530

    Update database.sql

--- vicinityservice/db/database.sql
+++ vicinityservice/db/database.sql
@@ -12,8 +12,8 @@ CREATE TABLE `chatbot_hints` (
 INSERT INTO `chatbot_hints` (`id`, `question`, `reply`) VALUES
 (1, 'HI||Hello||Hola', 'Hello, how are you.'),
 (2, 'How are you', 'Good to see you again!'),
-(3, 'what is your name||whats your name', 'My name is Vishal Bot'),
-(4, 'what should I call you', 'You can call me Vishal Bot'),
+(3, 'what is your name||whats your name', 'My name is Special Bot'),
+(4, 'what should I call you', 'You can call me  Bot'),
 (5, 'Where are your from', 'I m from India'),
 (6, 'Bye||See you later||Have a Good Day', 'Sad to see you are going. Have a nice day');
 
@@ -35,16 +35,7 @@ CREATE TABLE `message` (
 --
 
 INSERT INTO `message` (`id`, `message`, `added_on`, `type`) VALUES
-(1, 'Hi', '2020-04-22 12:41:04', 'user'),
-(2, 'Hello, how are you.', '2020-04-22 12:41:05', 'bot'),
-(3, 'what is your name', '2020-04-22 12:41:22', 'user'),
-(4, 'My name is Vishal Bot', '2020-04-22 12:41:22', 'bot'),
-(5, 'Where are your from', '2020-04-22 12:41:30', 'user'),
-(6, 'I m from India', '2020-04-22 12:41:30', 'bot'),
-(7, 'Go to hell', '2020-04-22 12:41:41', 'user'),
-(8, 'Sorry not be able to understand you', '2020-04-22 12:41:41', 'bot'),
-(9, 'bye', '2020-04-22 12:41:46', 'user'),
-(10, 'Sad to see you are going. Have a nice day', '2020-04-22 12:41:46', 'bot');
+();
 
 --
 -- Indexes for dumped tables
